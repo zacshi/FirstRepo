d2346c293a28b1e42afb0a73e3982dc3669afdab
zacshi <zacshi@users.noreply.github.com>
2016-01-08 20:58:01 -0500

    Update sourceCode.py

--- sourceCode.py
+++ sourceCode.py
@@ -1,1 +1,5 @@
-Not his no code here
+#print 96well plate labels
+for Let in range(65, 73): # Step through charactor number 65 to 72 in ASCII
+    for num in range(1, 13):
+        print chr(Let) + str(num),# teh comma seems to be giving a space instead of new line
+    print 
